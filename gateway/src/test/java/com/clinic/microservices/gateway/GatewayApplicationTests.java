3c2f1282fed85830f5f171a5589666a0e8b29da8
Alejandro Villafaña <0258447@up.edu.mx>
2025-11-25 08:42:17 -0600

    gateway mircoservice, creation

---
+++ gateway/src/test/java/com/clinic/microservices/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.microservices.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
